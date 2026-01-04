89ec9f106c758e00eb658bbcfc6893a58008b483
vojtechryp <vojtech@ryp.cz>
2026-01-04 17:39:11 +0100

    COMMIT 1: Zakladni struktura projektu a Program.cs

--- Threads-ParallelFileAnalyzer/Program.cs
+++ Threads-ParallelFileAnalyzer/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
-using System.Threading;
 
 namespace ParallelProcessor
 {
+    /// <summary>
+    /// Hlavni vstupni bod programu
+    /// Jen spousti analyzu - samotna logika je v jinych tridach
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Parallel File Analyzer");
-            Console.WriteLine("Hlavni thread: " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("=== Parallel File Analyzer ===");
+            Console.WriteLine("Program pro paralelni analyzu textovych souboru");
+            Console.WriteLine();
+            
+            // TODO: Zde vytvorime FileAnalyzer a spustime analyzu
+            // To udelame v dalsich commitech
+            
+            Console.WriteLine("Zatim nic nedelame, ale brzy budeme!");
         }
     }
 }
