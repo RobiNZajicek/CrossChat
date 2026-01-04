2eb6a8a51f3ff9faea2936791899816e4b0d63a5
vojtechryp <vojtech@ryp.cz>
2026-01-04 20:55:55 +0100

    COMMIT 6: TOP 10 nejcastejsich slov + mereni casu

--- Threads-ParallelFileAnalyzer/FileAnalyzer.cs
+++ Threads-ParallelFileAnalyzer/FileAnalyzer.cs
@@ -30,30 +30,29 @@ namespace ParallelProcessor
             _logger.Log($"Pocet threadu: {_threadCount}");
             _logger.Log(new string('-', 40));
 
-            // pripravime pole pro vysledky
             _results = new AnalysisResult[_threadCount];
             
             // vytvorime a spustime thready
+            var startTime = DateTime.Now;
             Thread[] threads = CreateThreads(filePath, fileSize, chunkSize);
             StartThreads(threads);
             WaitForThreads(threads);
+            var elapsed = DateTime.Now - startTime;
 
             // agregace a vypis vysledku
-            PrintResults();
+            PrintResults(elapsed);
         }
 
-        // vytvori thready - kazdy dostane svoji cast souboru
         private Thread[] CreateThreads(string filePath, long fileSize, long chunkSize)
         {
             Thread[] threads = new Thread[_threadCount];
 
             for (int i = 0; i < _threadCount; i++)
             {
-                int threadId = i;  // dulezite! kopie pro closure
+                int threadId = i;
                 long startPos = i * chunkSize;
                 long endPos = (i == _threadCount - 1) ? fileSize : (i + 1) * chunkSize;
 
-                // vytvorime processor a thread
                 var processor = new ChunkProcessor(filePath, startPos, endPos, _logger);
                 
                 threads[i] = new Thread(() => {
@@ -65,45 +64,66 @@ namespace ParallelProcessor
             return threads;
         }
 
-        // spusti vsechny thready naraz
         private void StartThreads(Thread[] threads)
         {
             foreach (var thread in threads)
-            {
                 thread.Start();
-            }
         }
 
-        // pocka az vsechny thready skonci
         private void WaitForThreads(Thread[] threads)
         {
             foreach (var thread in threads)
-            {
                 thread.Join();
-            }
         }
 
-        // spocita a vypise vysledky
-        private void PrintResults()
+        private void PrintResults(TimeSpan elapsed)
         {
             _logger.Log(new string('-', 40));
             _logger.Log("=== VYSLEDKY ===");
 
             int totalWords = 0;
             int totalLines = 0;
+            var totalFrequency = new Dictionary<string, int>();
 
-            // secteme vysledky od vsech threadu
+            // secteme vysledky a sloucime frekvence
             for (int i = 0; i < _threadCount; i++)
             {
                 var r = _results[i];
                 _logger.Log($"Thread-{i + 1}: {r.LineCount} radku, {r.WordCount} slov");
                 totalWords += r.WordCount;
                 totalLines += r.LineCount;
+
+                // sloucime slovniky frekvenci
+                foreach (var kvp in r.WordFrequency)
+                {
+                    if (!totalFrequency.ContainsKey(kvp.Key))
+                        totalFrequency[kvp.Key] = 0;
+                    totalFrequency[kvp.Key] += kvp.Value;
+                }
             }
 
             _logger.Log(new string('-', 40));
             _logger.Log($"CELKEM: {totalLines} radku, {totalWords} slov");
+            _logger.Log($"Cas: {elapsed.TotalMilliseconds:F0} ms");
+
+            // TOP 10 nejcastejsich slov
+            PrintTopWords(totalFrequency, 10);
+        }
+
+        // seradi slova podle frekvence a vypise TOP N
+        private void PrintTopWords(Dictionary<string, int> frequency, int count)
+        {
+            _logger.Log("");
+            _logger.Log("=== TOP 10 NEJCASTEJSICH SLOV ===");
+
+            // prevedeme na list a seradime sestupne
+            var sorted = new List<KeyValuePair<string, int>>(frequency);
+            sorted.Sort((a, b) => b.Value.CompareTo(a.Value));
+
+            for (int i = 0; i < Math.Min(count, sorted.Count); i++)
+            {
+                _logger.Log($"{i + 1}. \"{sorted[i].Key}\" - {sorted[i].Value}x");
+            }
         }
     }
 }
-
