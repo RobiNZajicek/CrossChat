c29129ef916c9ef42b124f3f70ee69b5f8fa0494
vojtechryp <vojtech@ryp.cz>
2026-01-04 17:44:42 +0100

    COMMIT 1: ConsoleLogger - thread-safe vypis do konzole

--- Threads-ParallelFileAnalyzer/ConsoleLogger.cs
+++ Threads-ParallelFileAnalyzer/ConsoleLogger.cs
@@ -3,51 +3,28 @@ using System.Threading;
 
 namespace ParallelProcessor
 {
-    /// <summary>
-    /// Thread-safe logger pro vypis do konzole.
-    /// Pouziva LOCK aby se vypisy z ruznych threadu nepromichaly.
-    /// 
-    /// PROC TO POTREBUJEME:
-    /// - Kdyz vic threadu pise do konzole naraz, text se muze promichat
-    /// - Lock zajisti ze vzdy pise jen JEDEN thread
-    /// - Ostatni thready cekaji az se zamek uvolni
-    /// </summary>
+    // trida pro bezpecny vypis do konzole z vice threadu
     public class ConsoleLogger
     {
-        // zamek - objekt ktery slouzi jako "klic" k zamykani
-        // readonly = po vytvoreni se nemuze zmenit
+        // zamek - jen jeden thread muze vypisovat naraz
         private readonly object _lock = new object();
 
-        /// <summary>
-        /// Vypise zpravu s casovou znackou.
-        /// Je THREAD-SAFE - muze byt volana z vice threadu naraz.
-        /// </summary>
-        /// <param name="message">Zprava k vypsani</param>
+        // vypise zpravu s casovou znackou
         public void Log(string message)
         {
-            // LOCK blok - pouze jeden thread muze byt uvnitr
-            // ostatni thready CEKAJI pred zamkem
             lock (_lock)
             {
-                // cas ve formatu HH:mm:ss.fff (hodiny:minuty:sekundy.milisekundy)
-                string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
-                Console.WriteLine($"[{timestamp}] {message}");
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {message}");
             }
-            // zde se zamek AUTOMATICKY uvolni
         }
 
-        /// <summary>
-        /// Vypise zpravu s nazvem aktualniho threadu.
-        /// Uzitecne pro sledovani ktery thread co dela.
-        /// </summary>
-        /// <param name="message">Zprava k vypsani</param>
+        // vypise zpravu s nazvem aktualniho threadu
         public void LogWithThread(string message)
         {
             lock (_lock)
             {
-                string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
                 string threadName = Thread.CurrentThread.Name ?? $"Thread-{Thread.CurrentThread.ManagedThreadId}";
-                Console.WriteLine($"[{timestamp}] [{threadName}] {message}");
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] [{threadName}] {message}");
             }
         }
     }
--- Threads-ParallelFileAnalyzer/Program.cs
+++ Threads-ParallelFileAnalyzer/Program.cs
@@ -2,24 +2,15 @@
 
 namespace ParallelProcessor
 {
-    /// <summary>
-    /// Hlavni vstupni bod programu
-    /// Jen spousti analyzu - samotna logika je v jinych tridach
-    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            // vytvorime logger - bude sdilet vsechny thready
+            // vytvorime logger - bude sdileny mezi thready
             var logger = new ConsoleLogger();
             
             logger.Log("=== Parallel File Analyzer ===");
-            logger.Log("Program pro paralelni analyzu textovych souboru");
-            
-            // ukazka logovani s nazvem threadu
-            logger.LogWithThread("Toto je hlavni thread");
-            
-            // TODO: Zde vytvorime FileAnalyzer a spustime analyzu
+            logger.LogWithThread("Program spusten");
         }
     }
 }
